425dc1400ce60ed0e1815d68c124c04f63cdb61d
Phill Anderson <eaglesquad86@gmail.com>
2020-05-11 18:44:35 +0800

    Бүх категорийг авна, нэг категорийг авна

--- controller/categories.js
+++ controller/categories.js
@@ -1,16 +1,39 @@
 const Category = require("../models/Category");
 // контроллер функцуудээ middleware хэлбэрээр бичнэ
-exports.getCategories = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: "Бүх категориудыг энд өгнө",
-  });
+exports.getCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json({
+      success: true,
+      data: categories,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      error: err,
+    });
+  }
 };
-exports.getCategory = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: `${req.params.id} id- тэй категорийн мэдээллийг өгнө`,
-  });
+exports.getCategory = async (req, res, next) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (category === null) {
+      res.status(400).json({
+        success: false,
+        error: req.params.id + " id-тэй категор байхгүй.",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: category,
+      });
+    }
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      error: err,
+    });
+  }
 };
 exports.createCategory = async (req, res, next) => {
   console.log("нэмэх утга :" + JSON.stringify(req.body));
