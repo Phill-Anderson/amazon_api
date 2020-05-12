e02e28b7c26c00ccbad666a5d5bf2a1054167a93
Phill Anderson <eaglesquad86@gmail.com>
2020-05-12 14:24:39 +0800

    Алдааны мэссэжийг монголоор харуулах

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,21 @@
 const errorHandler = (err, req, res, next) => {
   console.log(err.stack.underline.cyan);
+
+  // орж ирсэн алдааны обьектынхоо хуулбар обьектыг үүсгэж байна
+  const error = { ...err };
+
+  if (error.name === "CastError") {
+    error.message = "Энэ ID буруу бүтэцтэй ID байна";
+    error.statusCode = 400;
+  }
+  if (error.code === 11000) {
+    error.message = "Энэ талбарын утгыг давхардуулж өгч болохгүй";
+    error.statusCode = 400;
+  }
+
   res.status(err.statusCode || 500).json({
     success: false,
-    error: err.message,
+    error: error,
   });
 };
 module.exports = errorHandler;
